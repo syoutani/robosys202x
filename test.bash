61c27bdc30c0dd3878430b842fb5908e0b6b377d
syoutani <s21c1079pj@s.chibakoudai.jp>
2022-10-27 12:45:40 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 # SPDX-FileCoppyright: 2022 Tanimoto Yuuki
 # SPDX-License-Identifier: BSD-3-Clause
 ng () {
@@ -11,5 +11,14 @@ res=0
 ### I/0 TEST ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
+### STRANGE INPUT###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
 [ "$res" = 0 ] && echo OK
 exit $res
